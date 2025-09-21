dfbaec6e61f744e6239e5713b941bd477b220a40
Nhan Gia Minh <giaminh.nhan@kabam.ai>
2025-09-21 14:40:17 +0800

    change image pub qos to sensor data

--- tello_driver/src/tello_driver_node.cpp
+++ tello_driver/src/tello_driver_node.cpp
@@ -35,7 +35,7 @@ namespace tello_driver
     Node("tello_driver", options)
   {
     // ROS publishers
-    image_pub_ = create_publisher<sensor_msgs::msg::Image>("image_raw", 1);
+    image_pub_ = create_publisher<sensor_msgs::msg::Image>("image_raw", rclcpp::SensorDataQoS());
     camera_info_pub_ = create_publisher<sensor_msgs::msg::CameraInfo>("camera_info", rclcpp::SensorDataQoS());
     flight_data_pub_ = create_publisher<tello_msgs::msg::FlightData>("flight_data", 1);
     tello_response_pub_ = create_publisher<tello_msgs::msg::TelloResponse>("tello_response", 1);
